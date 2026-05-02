d1c0eb7707568bddcf145b14142840410bf14ccd
Juraj Pavle <jp@Jurajs-MacBook-Pro.local>
2026-05-02 22:25:35 +0200

    Add extensive logging for debugging multiplayer

--- cat-mmo/server.js
+++ cat-mmo/server.js
@@ -12,33 +12,34 @@ let players = {}; // Shared player state: { id: { position, rotation, ... } }
 
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
-
-  // Initialize new player with default state
   players[socket.id] = { position: { x: 0, y: 0, z: 0 }, rotation: 0 };
+  console.log('Players after connect:', Object.keys(players));
 
-  // Send init with player ID
   socket.emit('init', { id: socket.id });
+  console.log('Sent init to', socket.id);
 
-  // Broadcast new player to others
   socket.broadcast.emit('playerJoined', { id: socket.id, state: players[socket.id] });
+  console.log('Broadcasted playerJoined for', socket.id);
 
-  // Send current players to new player
   socket.emit('worldUpdate', players);
+  console.log('Sent worldUpdate to', socket.id, 'with players:', players);
 
   socket.on('command', (data) => {
-    // Update local player state (assume data has position/rotation updates)
+    console.log('Received command from', socket.id, ':', data);
     if (players[socket.id]) {
       players[socket.id] = { ...players[socket.id], ...data };
+      console.log('Updated players:', players);
     }
-    // Broadcast updated world state to all clients
     io.emit('worldUpdate', players);
+    console.log('Broadcasted worldUpdate to all');
   });
 
   socket.on('disconnect', () => {
     console.log('User disconnected:', socket.id);
     delete players[socket.id];
-    // Broadcast removal
+    console.log('Players after disconnect:', Object.keys(players));
     socket.broadcast.emit('playerLeft', { id: socket.id });
+    console.log('Broadcasted playerLeft for', socket.id);
   });
 });
 
